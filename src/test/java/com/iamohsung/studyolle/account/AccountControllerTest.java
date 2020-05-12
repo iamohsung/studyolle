13bc4f1ed57ebd09a1add3bb0dfdc200c720b4c4
David <blunfeel@gmail.com>
2020-05-12 13:22:59 +0900

    회원가입 뷰

--- src/test/java/com/iamohsung/studyolle/account/AccountControllerTest.java
+++ src/test/java/com/iamohsung/studyolle/account/AccountControllerTest.java
@@ -7,10 +7,9 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -18,12 +17,13 @@ public class AccountControllerTest {
 
     @Autowired private MockMvc mockMvc;
 
-    @DisplayName("회원 가입 화면 보여지는 테스트")
+    @DisplayName("회원 가입 화면 보이는지 테스트")
     @Test
     void signUpForm() throws Exception {
         mockMvc.perform(get("/sign-up"))
                 .andDo(print())
                 .andExpect(status().isOk())
-                .andExpect(view().name("account/sign-up"));
+                .andExpect(view().name("account/sign-up"))
+                .andExpect(model().attributeExists("signUpForm"));
     }
 }
