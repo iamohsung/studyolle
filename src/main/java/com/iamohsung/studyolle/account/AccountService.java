77e0456006a50c9f9d6a6b2a3243567dbc6ab8c6
David <blunfeel@gmail.com>
2020-05-12 15:09:37 +0900

    회원가입 리팩토링 및 테스트

---
+++ src/main/java/com/iamohsung/studyolle/account/AccountService.java
@@ -0,0 +1,47 @@
+package com.iamohsung.studyolle.account;
+
+import com.iamohsung.studyolle.domain.Account;
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+import javax.validation.Valid;
+
+@Service
+@RequiredArgsConstructor
+public class AccountService {
+    private final AccountRepository accountRepository;
+    private final JavaMailSender javaMailSender;
+
+    public void processNewAccount(SignUpForm signUpForm) {
+        Account newAccount = saveNewAccount(signUpForm);
+
+        newAccount.generateEmailCheckToken();
+        SimpleMailMessage mailMessage = sendSignUpConfirmEmail(newAccount);
+    }
+
+    private Account saveNewAccount(@Valid SignUpForm signUpForm) {
+        Account account = Account.builder()
+                .email(signUpForm.getEmail())
+                .nickname(signUpForm.getNickname())
+                .password(signUpForm.getPassword())
+                .studyCreatedByWeb(true)
+                .studyEnrollmentResultByWeb(true)
+                .studyUpdatedByWeb(true)
+                .build();
+
+        return accountRepository.save(account);
+    }
+
+    private SimpleMailMessage sendSignUpConfirmEmail(Account newAccount) {
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setTo(newAccount.getEmail());
+        mailMessage.setSubject("스터디올레 회원 가입 인증");
+        mailMessage.setText("/check-email-token?token=" + newAccount.getEmailCheckToken() + "&email=" + newAccount.getEmail());
+
+        javaMailSender.send(mailMessage);
+
+        return mailMessage;
+    }
+}
